b075b99f67e06c7a89739c0fcceb6cf458da64ea
Wachirapon Phanphueng <Khaning_wcrp012@hotmail.com>
2022-02-17 16:04:26 +0700

    add intro-to-js

--- intro-to-js/khanink.js
+++ intro-to-js/khanink.js
@@ -1,12 +1,14 @@
 function askMyName() {
     const name = prompt("Please enter your name");
-     // เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
+    // เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
  
     if (name != null) {
-       document.getElementById("name").innerHTML = "My name is " + name;
-    } }
+       document.getElementById("ใส่ id ของ h1 ในนี้").innerHTML = "My name is " + name;
+    }
+ }
  
  function helloWorld() {
-    const h1message = document.getElementById('name').innerHTML
-       alert(h1message + 'ถ้ารีบขนาดนี้ทำไมไม่มาตั้งแต่เมื่อวาน');
-    }
+ const h1message = document.getElementById("name").innerHTML
+//  ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
+    alert(h1message + 'ถ้ารีบขนาดนี้ทำไมไม่มาตั้งแต่เมื่อวาน')
+ }
