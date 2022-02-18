33694472e42f21bb67680953491e5fd1f3528175
Wachirapon Phanphueng <Khaning_wcrp012@hotmail.com>
2022-02-18 14:06:28 +0700

    add intro-to-js

--- intro-to-js/khanink.js
+++ intro-to-js/khanink.js
@@ -10,3 +10,19 @@ function helloWorld() {
    const h1message = document.getElementById("askMyName").innerHTML // ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
    alert(h1message + 'ถ้ารีบขนาดนี้ทำไมไม่มาตั้งแต่เมื่อวาน')
 }
+
+
+function calAge() {
+   const Yofbirth = prompt("Please enter your year of birth");
+   const date = new Date()
+   let year = date.getFullYear()
+   const age = year - Yofbirth;
+   alert("ยินดีด้วย นายอยู่ในประเทศนี้มา"+ age +"ปีแล้ว")
+}
+
+function loadProfile() {
+   const MyName = prompt("Please enter your name");
+   const Province = prompt("Please enter your province");
+   document.getElementById("my-name").innerHTML = MyName;
+   document.getElementById("my-province").innerHTML = Province;
+}
